2efbb178527cb8518568239f3e1036e3da290104
Fyodor Yarochkin <fyodor@p1sec.com>
2011-12-15 14:04:43 +0800

    Use rabbitmq queues

---
+++ config/amqp.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url: "amqp://localhost",
+    exchange: 'log'
+}
---
+++ server2.js
@@ -0,0 +1,11 @@
+// This file is the entry point for no.de
+//var web = require("./lib/web/server"),
+//    syslog = require("./lib/syslog/server")
+	amqp_syslog = require("./lib/syslog/server_amqp")
+
+
+// Use the supplied app-server port
+//web.listen(process.env.PORT || 8001)
+
+// Bind to a really high port so sudo is not needed
+//syslog.bind(10514)
