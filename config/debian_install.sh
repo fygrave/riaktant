149a44b049b384fe513f7b60cb8b13c1c2d4ddef
Fyodor Y <fygrave@o0o.nu>
2012-11-30 20:48:31 +0800

    installer

--- config/debian_install.sh
+++ config/debian_install.sh
@@ -1,24 +1,36 @@
 #!/bin/sh
 
-apt-get install build-essential openjdk-6-jdk  openssl libssl-dev pkg-config curl libcurl4-openssl-dev php5-curl php5-pecl php5-memcache memcached supervisor git
-wget http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.4/rabbitmq-server_2.8.4-1_all.deb
-dpkg -i rabbitmq-server_2.8.4-1_all.deb
+curl riak
+curl rabbitmq
+curl solr
+curl http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.4/rabbitmq-server_2.8.4-1_all.deb -o rabbitmq-server_2.8.4-1_all.deb
+#wget http://nodejs.org/dist/v0.8.4/node-v0.8.4.tar.gz
+#tar xvfz node-v0.8.4.tar.gz
+#cd node-v0.8.4
+#./configure
+#make
+#make install
+#curl http://npmjs.org/install.sh | sh
+#cd ..
+apt-get install build-essential openjdk-6-jdk  libssl-dev pkg-config curl libcurl4-openssl-dev php5-curl php5-pecl php5-memcache memcached supervisor git
 apt-get install erlang-nox
-apt-get -f install
-rabbitmq-plugins enable rabbitmq_management
-wget http://nodejs.org/dist/v0.8.4/node-v0.8.4.tar.gz
-tar xvfz node-v0.8.4.tar.gz
-cd node-v0.8.4
-./configure
-make
-make install
-curl http://npmjs.org/install.sh | sh
-cd ..
-
+apt-get install erlang-base
+apt-get install php-pecl
+apt-get install php-pear
+apt-get install libxml2-dev
+apt-get install libssl0.9.8
+apt-get install libssl0.9.8
+apt-get install munin munin-node
 pecl install -n solr
-
+apt-get -f install
+dpkg -i riak_1.1.2-1_amd64.deb
+tar xvfz node-v0.6.18.tar.gz
+cd node-v0.6.18/
+./configure && make && make install && supervisorctl restart all
+cd ..
+sudo curl http://npmjs.org/install.sh | sh
+rabbitmq-plugins enable rabbitmq_management
 npm install forever -g
-
 git clone http://github.com/fygrave/riaktant.git
 cd riaktant
 npm install
