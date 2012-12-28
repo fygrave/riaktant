1fc8e1cc9b5298fea6408844494c1eb257d28418
Fyodor Y <fygrave@o0o.nu>
2012-12-28 17:00:47 +0800

    adding -y switch

--- config/debian_install.sh
+++ config/debian_install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-apt-get install shorewall
-apt-get install openvpn
-apt-get install curl build-essential
+apt-get -y install shorewall
+apt-get -y install openvpn
+apt-get -y install curl build-essential
 [ -f riak_1.2.1-1_amd64.deb ] || wget http://downloads.basho.com.s3-website-us-east-1.amazonaws.com/riak/CURRENT/debian/6/riak_1.2.1-1_amd64.deb
 [ -f rabbitmq-server_2.8.4-1_all.deb ] || curl http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.4/rabbitmq-server_2.8.4-1_all.deb -o rabbitmq-server_2.8.4-1_all.deb
 [ -f apache-solr-4.0.0.tgz ] || curl  http://apache.cdpa.nsysu.edu.tw/lucene/solr/4.0.0/apache-solr-4.0.0.tgz -o apache-solr-4.0.0.tgz
@@ -13,20 +13,24 @@ tar xvfz apache-solr-4.0.0.tgz
 #./configure
 #make
 #make install
-apt-get install build-essential openjdk-6-jdk  libssl-dev pkg-config curl libcurl4-openssl-dev php5-curl php-pear php5-memcache memcached supervisor git
-apt-get install erlang-nox
-apt-get install erlang-base
-apt-get install php-pear
-apt-get install libxml2-dev
-apt-get install libssl0.9.8
-apt-get install libssl0.9.8
-apt-get install munin munin-node
+apt-get -y install build-essential openjdk-6-jdk  libssl-dev pkg-config curl libcurl4-openssl-dev php5-curl php-pear php5-memcache memcached supervisor git
+apt-get -y install erlang-nox
+apt-get -y install erlang-base
+apt-get -y install php-pear
+apt-get -y install libxml2-dev
+apt-get -y install libssl0.9.8
+apt-get -y install libssl0.9.8
+apt-get -y install munin munin-node
 pecl install -n solr
-apt-get -f install
+apt-get -y -f install
 dpkg -i riak_1.2.1-1_amd64.deb
-[ -f node-v0.6.18.tar.gz ] || wget http://nodejs.org/dist/v0.6.18/node-v0.6.18.tar.gz
-tar xvfz node-v0.6.18.tar.gz
-cd node-v0.6.18/
+#[ -f node-v0.6.18.tar.gz ] || wget http://nodejs.org/dist/v0.6.18/node-v0.6.18.tar.gz
+#tar xvfz node-v0.6.18.tar.gz
+#cd node-v0.6.18/
+
+[ -f node-v0.8.16.tar.gz ] || wget http://nodejs.org/dist/v0.8.16/node-v0.8.16.tar.gz
+tar xvfz node-v0.8.16.tar.gz
+cd node-v0.8.16/
 ./configure && make && make install && supervisorctl restart all
 cd ..
 curl https://npmjs.org/install.sh | sh
