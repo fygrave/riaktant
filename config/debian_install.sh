931d7ad580f909576086038ff9ef57a4d396d541
Fyodor Y <fygrave@o0o.nu>
2013-04-24 08:27:21 +0800

    install script mod

--- config/debian_install.sh
+++ config/debian_install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+SOLRV="4.2.1"
+RIAKV="1.2.1-1"
 apt-get -y install shorewall
 apt-get -y install openvpn
 apt-get -y install curl build-essential
-[ -f riak_1.2.1-1_amd64.deb ] || wget http://downloads.basho.com.s3-website-us-east-1.amazonaws.com/riak/CURRENT/debian/6/riak_1.2.1-1_amd64.deb
+[ -f riak_"$RIAKV"_amd64.deb ] || wget http://downloads.basho.com.s3-website-us-east-1.amazonaws.com/riak/CURRENT/debian/6/riak_"$RIAKV"_amd64.deb
 [ -f rabbitmq-server_2.8.4-1_all.deb ] || curl http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.4/rabbitmq-server_2.8.4-1_all.deb -o rabbitmq-server_2.8.4-1_all.deb
-[ -f apache-solr-4.0.0.tgz ] || curl  http://apache.cdpa.nsysu.edu.tw/lucene/solr/4.0.0/apache-solr-4.0.0.tgz -o apache-solr-4.0.0.tgz
-tar xvfz apache-solr-4.0.0.tgz
+[ -f apache-solr-$SOLRV.tgz ] || curl  http://apache.cdpa.nsysu.edu.tw/lucene/solr/$SOLRV/apache-solr-$SOLRV.tgz -o apache-solr-$SOLRV.tgz
+tar xvfz apache-solr-$SOLRV.tgz
 #wget http://nodejs.org/dist/v0.8.4/node-v0.8.4.tar.gz
 #tar xvfz node-v0.8.4.tar.gz
 #cd node-v0.8.4
@@ -22,12 +24,18 @@ apt-get -y install libxml2-dev
 apt-get -y install libssl0.9.8
 apt-get -y install libssl0.9.8
 apt-get -y install munin munin-node
+apt-get -y install python-pip
 pecl install -n solr
 apt-get -y -f install
-dpkg -i riak_1.2.1-1_amd64.deb
+dpkg -i riak_"$RIAKV"_amd64.deb
 #[ -f node-v0.6.18.tar.gz ] || wget http://nodejs.org/dist/v0.6.18/node-v0.6.18.tar.gz
 #tar xvfz node-v0.6.18.tar.gz
 #cd node-v0.6.18/
+pip install graphite-web warden
+pip install carbon
+pip install whisper
+
+[ -f elasticsearch-0.20.6.tar.gz ] || wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.6.tar.gz
 
 [ -f node-v0.8.16.tar.gz ] || wget http://nodejs.org/dist/v0.8.16/node-v0.8.16.tar.gz
 tar xvfz node-v0.8.16.tar.gz
@@ -42,7 +50,7 @@ git clone http://github.com/fygrave/riaktant.git
 cd riaktant
 npm install
 cd ..
-cp riaktant/config/schema.xml apache-solr-4.0.0/example/solr/collection1/conf/schema.xml
+cp riaktant/config/schema.xml apache-solr-$SOLRV/example/solr/collection1/conf/schema.xml
 cp riaktant/config/supervisor_master.conf /etc/supervisor/conf.d/
 /etc/init.d/apache2 restart
 /etc/init.d/rabbitmq-server restart
