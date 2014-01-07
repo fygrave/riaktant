ec1571beabaf19b049a77983bf57f16e6387c210
Fyodor Y <fygrave@o0o.nu>
2014-01-07 16:39:53 +0800

    config to zmq

--- config/amqp.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    url: "amqp://localhost",
-    exchange: 'log',
-    work_queue: 'log-tasks',
-    cep_queue: 'cep-tasks'
-}
---
+++ config/zmq.js
@@ -0,0 +1,4 @@
+module.exports = {
+    server: "tcp://127.0.0.1:3000",
+
+}
